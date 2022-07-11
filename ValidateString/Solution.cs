14716999aad4c1bb3b25f9bbcf5665f414640d3f
xsurfer <perfabio87@gmail.com>
2022-07-11 19:38:05 +0200

    Add project files.

---
+++ ValidateString/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidateString
+{
+    class Solution
+    {
+        public static bool ValidateString(String toValidate) {
+            // implement your solution here
+            return false;
+        }
+    }
+}
---
+++ ValidateString/ValidateStringFuncApp.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ValidateString
+{
+    public static class ValidateStringFuncApp
+    {
+        [FunctionName("ValidateStringFuncApp")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseMessage = "No string received";
+            string stringToValidate = req.Query["toValidate"];
+            if (stringToValidate != null)
+            {
+                bool isStringValid = Solution.ValidateString(stringToValidate);
+
+                responseMessage = isStringValid
+                    ? "String is valid"
+                    : "String is not valid";
+            }
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
