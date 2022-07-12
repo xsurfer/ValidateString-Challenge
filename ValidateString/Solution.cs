16be0984d40488ce5755763ce8e3fcaac7ff078f
xsurfer <perfabio87@gmail.com>
2022-07-12 15:52:03 +0200

    introduce enhancements and test cases

--- ValidateString/Solution.cs
+++ ValidateString/Solution.cs
@@ -7,46 +7,26 @@ namespace ValidateString
 {
 	public class Solution
 	{
+		/// <summary>
+		/// Given a string S consisting of N characters, returns true if S is properly nested and false otherwise.
+		/// A string S consisting of N characters is considered to be properly nested if any of the following conditions is true:
+		/// <list type="bullet">
+		/// <item>S is empty;</item>
+		/// <item>S has the form "(U)" or "[U]" or "{U}" where U is a properly nested string;</item>
+		/// <item>S has the form "VW" where V and W are properly nested strings.</item>
+		/// </list>
+		/// For example, given S = "{[()()]}", the function should return true and given S = "([)()]", the function should return false, as explained above.
+		/// Write an efficient algorithm for the following assumptions:
+		/// <list type="bullet">
+		/// <item>N is an integer within the range[0..200,000];</item>
+		/// <item>string S consists only of the following characters: "(", "{", "[", "]", "}" and/or ")".</item>
+		/// </list>
+		/// </summary>
+		/// <param name="toValidate"></param>
 		public static bool ValidateString(String toValidate)
 		{
-			Stack<char> stack = new Stack<char>();
-
-			foreach (char c in toValidate)
-			{
-				if (IsOpeningBracket(c))		
-					stack.Push(c);
-				else
-				{
-					if (stack.Count == 0)
-						return false;
-					char stackOpenBracket = stack.Pop();
-					if (MatchBracket(stackOpenBracket, c))
-						continue;
-					else
-						return false;
-				}
-			}
-			if (stack.Count == 0)
-				return true;
+			//implement here your solution
 			return false;
-		}
-
-		public static bool IsOpeningBracket(char c)
-		{
-			if (c.Equals('(') | c.Equals('[') | c.Equals('{'))
-				return true;
-			return false;
-		}
-
-		public static bool MatchBracket(char openBracket, char closedBracket)
-		{
-			if (closedBracket.Equals(')') && openBracket.Equals('('))
-				return true;
-			if (closedBracket.Equals(']') && openBracket.Equals('['))
-				return true;
-			if (closedBracket.Equals('}') && openBracket.Equals('{'))
-				return true;
-			return false;
-		}
+		}		
 	}
 }
--- ValidateStringTest/SolutionTest.cs
+++ ValidateStringTest/SolutionTest.cs
@@ -6,45 +6,16 @@ namespace ValidateStringTest
     [TestClass]
     public class SolutionTest
     {
-        [TestMethod]
-        public void IsOpeningBracket_ValidBrackets()
-        {
-            Assert.IsTrue(Solution.IsOpeningBracket('('));
-            Assert.IsTrue(Solution.IsOpeningBracket('['));
-            Assert.IsTrue(Solution.IsOpeningBracket('{'));
-        }
-
-        [TestMethod]
-        public void IsOpeningBracket_NotABracket()
-        {
-            Assert.IsFalse(Solution.IsOpeningBracket('2'));
-            Assert.IsFalse(Solution.IsOpeningBracket('b'));
-            Assert.IsFalse(Solution.IsOpeningBracket('%'));
-        }
-
-        [TestMethod]
-        public void MatchBracket_ValidBrackets()
-        {
-            Assert.IsTrue(Solution.MatchBracket('(',')'));
-            Assert.IsTrue(Solution.MatchBracket('[', ']'));
-            Assert.IsTrue(Solution.MatchBracket('{', '}'));
-        }
-
-        [TestMethod]
-        public void MatchBracket_NotValidBrackets()
-        {
-            Assert.IsFalse(Solution.MatchBracket('(', ']'));
-            Assert.IsFalse(Solution.MatchBracket('[', ')'));
-            Assert.IsFalse(Solution.MatchBracket('{', ']'));
-        }
-
+ 
         [TestMethod]
         public void ValidateString_ValidStrings()
         {
+            Assert.IsTrue(Solution.ValidateString(""));
             Assert.IsTrue(Solution.ValidateString("()"));
             Assert.IsTrue(Solution.ValidateString("[]"));
             Assert.IsTrue(Solution.ValidateString("{}"));
             Assert.IsTrue(Solution.ValidateString("([{{}}])"));
+            Assert.IsTrue(Solution.ValidateString("{[()]}[](){}"));
         }
 
         [TestMethod]
@@ -53,6 +24,8 @@ namespace ValidateStringTest
             Assert.IsFalse(Solution.ValidateString("([)"));
             Assert.IsFalse(Solution.ValidateString("[([)]"));
             Assert.IsFalse(Solution.ValidateString("]["));
+            Assert.IsFalse(Solution.ValidateString("]"));
+            Assert.IsFalse(Solution.ValidateString("["));
         }
 
     }
